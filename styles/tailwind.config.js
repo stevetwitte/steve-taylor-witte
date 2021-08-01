6692cc64b484c371e5ef2d398d74fa95333819a3
Stephen Taylor Witte <stevetwitte@gmail.com>
2021-08-01 13:24:26 -0400

    Initial commit

---
+++ styles/tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  purge: {
+    content: ['_site/**/*.html'],
+    options: {
+      safelist: [],
+    },
+  },
+  theme: {
+    fontFamily: {
+      sans: ['Fira Sans', 'sans-serif'],
+    },
+  },
+  variants: {},
+  plugins: [],
+}
